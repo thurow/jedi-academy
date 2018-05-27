72028c42fe5d49b45cbadc6ecc4cc7c80b6e4c43
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2018-05-27 17:30:33 -0300

    using decorator @input

---
+++ src/app/student/student.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'jad-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+  @Input() name: string
+  @Input() isJedi: boolean
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
